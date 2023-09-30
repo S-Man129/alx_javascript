9cf1d4d519515ca63fcf8205c251b15efd642b31
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-09-30 07:22:30 +0100

    8. Increment object new

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,18 +1,21 @@
 #!/usr/bin/node
+
 const myObject = {
     type: 'object',
-    value: 12
+    value: 12,
+    incr: function() {
+      this.value++;
+    }
   };
-  console.log(myObject);
   
-  myObject.incr = function() {
-    this.value++;
-  };
+  console.log(myObject);
   
   myObject.incr();
   console.log(myObject);
+  
   myObject.incr();
   console.log(myObject);
+  
   myObject.incr();
   console.log(myObject);
   
