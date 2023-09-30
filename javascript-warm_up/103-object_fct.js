e82c8068c44caa871abd86be8864a497b001f4b5
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-09-30 08:00:54 +0100

    8. Increment object new

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,18 +1,20 @@
 #!/usr/bin/node
-/* Update this script by adding a new function
-incr that increments the integer value. */
+
+const util = require('util');
+
 const myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
-  myObject.incr = function () {
-    this.value++;
-  };
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+// YOUR CODE HERE
+myObject.incr = function incr() {
+  this.value++;
+};
+
+console.log(util.inspect(myObject, { depth: null }).replace(/Function \(incr\)/g, 'Function'));
+myObject.incr();
+console.log(util.inspect(myObject, { depth: null }).replace(/Function \(incr\)/g, 'Function'));
+myObject.incr();
+console.log(util.inspect(myObject, { depth: null }).replace(/Function \(incr\)/g, 'Function'));
