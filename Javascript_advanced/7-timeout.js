6a5521b4365e17374a0c5a730cca66532fbf0def
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-09-30 13:24:33 +0100

    7. Stack order and setTimeout

--- Javascript_advanced/7-timeout.js
+++ Javascript_advanced/7-timeout.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
-// Log "Start of the execution queue"
 console.log("Start of the execution queue");
 
-// Schedule the final code block using setTimeout with a delay of 0
 setTimeout(function() {
   console.log("Final code block to be executed");
 }, 0);
@@ -13,5 +11,4 @@ for (let i = 1; i <= 100; i++) {
   console.log(i);
 }
 
-// Log "End of the loop printing"
 console.log("End of the loop printing");
