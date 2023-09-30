ce0fa7623d64eabec1df059cbdf1dcc93d3d7360
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-09-30 09:39:33 +0100

    0. Lexical scoping and welcome message

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+function welcome(firstName, lastName) {
+    const fullName = firstName + ' ' + lastName;
+  
+    function displayFullName() {
+      alert('Welcome ' + fullName + '!');
+    }
+  
+    displayFullName();
+  }
+  
