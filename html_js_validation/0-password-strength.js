f4bd88402a4196852b459e42905bfb51d97179c9
Sulayman Adebayo <adebayosulayman129@gmail.com>
2023-10-11 12:09:34 +0100

    0. Form Validation - Password Strength

---
+++ html_js_validation/0-password-strength.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Retrieve the form and error elements
+    var passwordForm = document.getElementById("passwordForm");
+    var errorElement = document.getElementById("error");
+
+    // Add a submit event listener to the form
+    passwordForm.addEventListener("submit", function (event) {
+        event.preventDefault(); // Prevent the default form submission
+
+        // Retrieve the password input value
+        var passwordInput = document.getElementById("password");
+        var password = passwordInput.value;
+
+        // Call the validatePassword function to check password strength
+        if (validatePassword(password)) {
+            // Password meets the criteria, clear the error message and submit the form
+            errorElement.textContent = "";
+            passwordForm.submit();
+        } else {
+            // Password does not meet the criteria, display an error message
+            errorElement.textContent = "Password does not meet the criteria.";
+        }
+    });
+
+    function validatePassword(password) {
+        // Define the password criteria
+        var lengthRegex = /.{8,}/;
+        var uppercaseRegex = /[A-Z]/;
+        var lowercaseRegex = /[a-z]/;
+        var digitRegex = /\d/;
+        var specialCharRegex = /[!@#$%^&*]/;
+
+        // Check if the password meets all criteria
+        return (
+            lengthRegex.test(password) &&
+            uppercaseRegex.test(password) &&
+            lowercaseRegex.test(password) &&
+            digitRegex.test(password) &&
+            specialCharRegex.test(password)
+        );
+    }
+});
